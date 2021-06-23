1db560be2901ae484559755db572f668eaf5d268
Danny SAUVAL <sauvaldanny@gmail.com>
2021-06-23 11:06:51 -0400

    Add posibility to copy-paste to a remote Windows.

---
+++ Libraries/VirtualDesktopLib/VirtualDesktopLib.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Media;
+
+// set attributes
+
+// Based on http://stackoverflow.com/a/32417530, Windows 10 SDK, github project Grabacr07/VirtualDesktop and own research
+
+namespace VirtualDesktopLib
+{
+	#region COM API
+	internal static class Guids
+	{
+		public static readonly Guid CLSID_ImmersiveShell = new Guid("C2F03A33-21F5-47FA-B4BB-156362A2F239");
+		public static readonly Guid CLSID_VirtualDesktopManagerInternal = new Guid("C5E0CDCA-7B6E-41B2-9FC4-D93975CC467B");
+		public static readonly Guid CLSID_VirtualDesktopManager = new Guid("AA509086-5CA9-4C25-8F95-589D3C07B48A");
+		public static readonly Guid CLSID_VirtualDesktopPinnedApps = new Guid("B5A399E7-1C87-46B8-88E9-FC5747B171BD");
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct Size
+	{
+		public int X;
+		public int Y;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct Rect
+	{
+		public int Left;
+		public int Top;
+		public int Right;
+		public int Bottom;
+	}
+
+	internal enum APPLICATION_VIEW_CLOAK_TYPE : int
+	{
+		AVCT_NONE = 0,
+		AVCT_DEFAULT = 1,
+		AVCT_VIRTUAL_DESKTOP = 2
+	}
+
+	internal enum APPLICATION_VIEW_COMPATIBILITY_POLICY : int
+	{
+		AVCP_NONE = 0,
+		AVCP_SMALL_SCREEN = 1,
+		AVCP_TABLET_SMALL_SCREEN = 2,
+		AVCP_VERY_SMALL_SCREEN = 3,
+		AVCP_HIGH_SCALE_FACTOR = 4
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+	[Guid("372E1D3B-38D3-42E4-A15B-8AB2B178F513")]
+	internal interface IApplicationView
+	{
+		int SetFocus();
+		int SwitchTo();
+		int TryInvokeBack(IntPtr /* IAsyncCallback* */ callback);
+		int GetThumbnailWindow(out IntPtr hwnd);
+		int GetMonitor(out IntPtr /* IImmersiveMonitor */ immersiveMonitor);
+		int GetVisibility(out int visibility);
+		int SetCloak(APPLICATION_VIEW_CLOAK_TYPE cloakType, int unknown);
+		int GetPosition(ref Guid guid /* GUID for IApplicationViewPosition */, out IntPtr /* IApplicationViewPosition** */ position);
+		int SetPosition(ref IntPtr /* IApplicationViewPosition* */ position);
+		int InsertAfterWindow(IntPtr hwnd);
+		int GetExtendedFramePosition(out Rect rect);
+		int GetAppUserModelId([MarshalAs(UnmanagedType.LPWStr)] out string id);
+		int SetAppUserModelId(string id);
+		int IsEqualByAppUserModelId(string id, out int result);
+		int GetViewState(out uint state);
+		int SetViewState(uint state);
+		int GetNeediness(out int neediness);
+		int GetLastActivationTimestamp(out ulong timestamp);
+		int SetLastActivationTimestamp(ulong timestamp);
+		int GetVirtualDesktopId(out Guid guid);
+		int SetVirtualDesktopId(ref Guid guid);
+		int GetShowInSwitchers(out int flag);
+		int SetShowInSwitchers(int flag);
+		int GetScaleFactor(out int factor);
+		int CanReceiveInput(out bool canReceiveInput);
+		int GetCompatibilityPolicyType(out APPLICATION_VIEW_COMPATIBILITY_POLICY flags);
+		int SetCompatibilityPolicyType(APPLICATION_VIEW_COMPATIBILITY_POLICY flags);
+		int GetSizeConstraints(IntPtr /* IImmersiveMonitor* */ monitor, out Size size1, out Size size2);
+		int GetSizeConstraintsForDpi(uint uint1, out Size size1, out Size size2);
+		int SetSizeConstraintsForDpi(ref uint uint1, ref Size size1, ref Size size2);
+		int OnMinSizePreferencesUpdated(IntPtr hwnd);
+		int ApplyOperation(IntPtr /* IApplicationViewOperation* */ operation);
+		int IsTray(out bool isTray);
+		int IsInHighZOrderBand(out bool isInHighZOrderBand);
+		int IsSplashScreenPresented(out bool isSplashScreenPresented);
+		int Flash();
+		int GetRootSwitchableOwner(out IApplicationView rootSwitchableOwner);
+		int EnumerateOwnershipTree(out IObjectArray ownershipTree);
+		int GetEnterpriseId([MarshalAs(UnmanagedType.LPWStr)] out string enterpriseId);
+		int IsMirrored(out bool isMirrored);
+		int Unknown1(out int unknown);
+		int Unknown2(out int unknown);
+		int Unknown3(out int unknown);
+		int Unknown4(out int unknown);
+		int Unknown5(out int unknown);
+		int Unknown6(int unknown);
+		int Unknown7();
+		int Unknown8(out int unknown);
+		int Unknown9(int unknown);
+		int Unknown10(int unknownX, int unknownY);
+		int Unknown11(int unknown);
+		int Unknown12(out Size size1);
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("1841C6D7-4F9D-42C0-AF41-8747538F10E5")]
+	internal interface IApplicationViewCollection
+	{
+		int GetViews(out IObjectArray array);
+		int GetViewsByZOrder(out IObjectArray array);
+		int GetViewsByAppUserModelId(string id, out IObjectArray array);
+		int GetViewForHwnd(IntPtr hwnd, out IApplicationView view);
+		int GetViewForApplication(object application, out IApplicationView view);
+		int GetViewForAppUserModelId(string id, out IApplicationView view);
+		int GetViewInFocus(out IntPtr view);
+		int Unknown1(out IntPtr view);
+		void RefreshCollection();
+		int RegisterForApplicationViewChanges(object listener, out int cookie);
+		int UnregisterForApplicationViewChanges(int cookie);
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("FF72FFDD-BE7E-43FC-9C03-AD81681E88E4")]
+	internal interface IVirtualDesktop
+	{
+		bool IsViewVisible(IApplicationView view);
+		Guid GetId();
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("F31574D6-B682-4CDC-BD56-1827860ABEC6")]
+	internal interface IVirtualDesktopManagerInternal
+	{
+		int GetCount();
+		void MoveViewToDesktop(IApplicationView view, IVirtualDesktop desktop);
+		bool CanViewMoveDesktops(IApplicationView view);
+		IVirtualDesktop GetCurrentDesktop();
+		void GetDesktops(out IObjectArray desktops);
+		[PreserveSig]
+		int GetAdjacentDesktop(IVirtualDesktop from, int direction, out IVirtualDesktop desktop);
+		void SwitchDesktop(IVirtualDesktop desktop);
+		IVirtualDesktop CreateDesktop();
+		void RemoveDesktop(IVirtualDesktop desktop, IVirtualDesktop fallback);
+		IVirtualDesktop FindDesktop(ref Guid desktopid);
+	}
+
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("A5CD92FF-29BE-454C-8D04-D82879FB3F1B")]
+	internal interface IVirtualDesktopManager
+	{
+		bool IsWindowOnCurrentVirtualDesktop(IntPtr topLevelWindow);
+		Guid GetWindowDesktopId(IntPtr topLevelWindow);
+		void MoveWindowToDesktop(IntPtr topLevelWindow, ref Guid desktopId);
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("4CE81583-1E4C-4632-A621-07A53543148F")]
+	internal interface IVirtualDesktopPinnedApps
+	{
+		bool IsAppIdPinned(string appId);
+		void PinAppID(string appId);
+		void UnpinAppID(string appId);
+		bool IsViewPinned(IApplicationView applicationView);
+		void PinView(IApplicationView applicationView);
+		void UnpinView(IApplicationView applicationView);
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("92CA9DCD-5622-4BBA-A805-5E9F541BD8C9")]
+	internal interface IObjectArray
+	{
+		void GetCount(out int count);
+		void GetAt(int index, ref Guid iid, [MarshalAs(UnmanagedType.Interface)] out object obj);
+	}
+
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("6D5140C1-7436-11CE-8034-00AA006009FA")]
+	internal interface IServiceProvider10
+	{
+		[return: MarshalAs(UnmanagedType.IUnknown)]
+		object QueryService(ref Guid service, ref Guid riid);
+	}
+	#endregion
+
+	#region COM wrapper
+	internal static class DesktopManager
+	{
+		static DesktopManager()
+		{
+			var shell = (IServiceProvider10)Activator.CreateInstance(Type.GetTypeFromCLSID(Guids.CLSID_ImmersiveShell));
+			VirtualDesktopManagerInternal = (IVirtualDesktopManagerInternal)shell.QueryService(Guids.CLSID_VirtualDesktopManagerInternal, typeof(IVirtualDesktopManagerInternal).GUID);
+			VirtualDesktopManager = (IVirtualDesktopManager)Activator.CreateInstance(Type.GetTypeFromCLSID(Guids.CLSID_VirtualDesktopManager));
+			ApplicationViewCollection = (IApplicationViewCollection)shell.QueryService(typeof(IApplicationViewCollection).GUID, typeof(IApplicationViewCollection).GUID);
+			VirtualDesktopPinnedApps = (IVirtualDesktopPinnedApps)shell.QueryService(Guids.CLSID_VirtualDesktopPinnedApps, typeof(IVirtualDesktopPinnedApps).GUID);
+		}
+
+		internal static IVirtualDesktopManagerInternal VirtualDesktopManagerInternal;
+		internal static IVirtualDesktopManager VirtualDesktopManager;
+		internal static IApplicationViewCollection ApplicationViewCollection;
+		internal static IVirtualDesktopPinnedApps VirtualDesktopPinnedApps;
+
+		internal static IVirtualDesktop GetDesktop(int index)
+		{   // get desktop with index
+			int count = VirtualDesktopManagerInternal.GetCount();
+			if (index < 0 || index >= count) throw new ArgumentOutOfRangeException("index");
+			IObjectArray desktops;
+			VirtualDesktopManagerInternal.GetDesktops(out desktops);
+			object objdesktop;
+			desktops.GetAt(index, typeof(IVirtualDesktop).GUID, out objdesktop);
+			Marshal.ReleaseComObject(desktops);
+			return (IVirtualDesktop)objdesktop;
+		}
+
+		internal static int GetDesktopIndex(IVirtualDesktop desktop)
+		{ // get index of desktop
+			int index = -1;
+			Guid IdSearch = desktop.GetId();
+			IObjectArray desktops;
+			VirtualDesktopManagerInternal.GetDesktops(out desktops);
+			object objdesktop;
+			for (int i = 0; i < VirtualDesktopManagerInternal.GetCount(); i++)
+			{
+				desktops.GetAt(i, typeof(IVirtualDesktop).GUID, out objdesktop);
+				if (IdSearch.CompareTo(((IVirtualDesktop)objdesktop).GetId()) == 0)
+				{
+					index = i;
+					break;
+				}
+			}
+			Marshal.ReleaseComObject(desktops);
+			return index;
+		}
+	}
+	#endregion
+
+	#region public interface
+	public class Desktop
+	{
+		private IVirtualDesktop ivd;
+		private Desktop(IVirtualDesktop desktop) { this.ivd = desktop; }
+
+		//USEFUL
+		public static int Count
+		{ // return the number of desktops
+			get { return DesktopManager.VirtualDesktopManagerInternal.GetCount(); }
+		}
+
+		//USEFUL
+		public static Desktop Current
+		{ // returns current desktop
+			get { return new Desktop(DesktopManager.VirtualDesktopManagerInternal.GetCurrentDesktop()); }
+		}
+
+		//USEFUL
+		public static Desktop FromIndex(int index)
+		{ // return desktop object from index (-> index = 0..Count-1)
+			return new Desktop(DesktopManager.GetDesktop(index));
+		}
+
+		//USEFUL
+		public static int FromDesktop(Desktop desktop)
+		{ // return index of desktop object or -1 if not found
+			return DesktopManager.GetDesktopIndex(desktop.ivd);
+		}
+
+		//USEFUL
+		public void MakeVisible()
+		{ // make this desktop visible
+			DesktopManager.VirtualDesktopManagerInternal.SwitchDesktop(ivd);
+		}
+		//USEFUL
+		public Desktop Left
+		{ // return desktop at the left of this one, null if none
+			get
+			{
+				IVirtualDesktop desktop;
+				int hr = DesktopManager.VirtualDesktopManagerInternal.GetAdjacentDesktop(ivd, 3, out desktop); // 3 = LeftDirection
+				if (hr == 0)
+					return new Desktop(desktop);
+				else
+					return null;
+			}
+		}
+		//USEFUL
+		public Desktop Right
+		{ // return desktop at the right of this one, null if none
+			get
+			{
+				IVirtualDesktop desktop;
+				int hr = DesktopManager.VirtualDesktopManagerInternal.GetAdjacentDesktop(ivd, 4, out desktop); // 4 = RightDirection
+				if (hr == 0)
+					return new Desktop(desktop);
+				else
+					return null;
+			}
+		}
+	}
+	#endregion
+}
---
+++ StreamDeck/StreamDeck.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO.Ports;
+using VirtualDesktopLib;
+using DisplayManagerLib;
+using AutoHotkey.Interop;
+
+using System.Media;
+
+namespace StreamDeck
+{
+    class StreamDeck
+    {
+        public static void Main(string[] args)
+        {
+            var ahk = AutoHotkeyEngine.Instance;
+
+            int side = 0;
+            int disp = 0;
+
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            var sp = new SerialPort(ports[0], 19200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+
+            sp.RtsEnable = true;
+            sp.DtrEnable = true;
+
+            sp.Open();
+            var readData = "";
+
+            char[] tab = new char[5]; // Too big
+
+            while (readData != "Stop received.\r")
+            {
+                try
+                {
+                    //readData = sp.ReadLine();
+                    if (sp.BytesToRead == 5)
+                    {
+                        sp.Read(tab, 0, 5);
+                        // Console.WriteLine(tab);
+
+                        switch (new string(tab)) // Ugly conversion
+                        {
+                            case "OPC0\n":
+                                if (side == 0)
+                                {
+                                    side = 1;
+                                    if (Desktop.FromDesktop(Desktop.FromIndex(Desktop.Count - 1)) != 0)
+                                    {
+                                        Desktop.Current.Right.MakeVisible();
+                                        Console.WriteLine("Switching to right virtual desktop");
+                                    }
+                                    else
+                                    {
+                                        SystemSounds.Exclamation.Play();
+                                        Console.WriteLine("No right virtual desktop to switch to.");
+                                    }
+                                }
+                                else if (side == 1)
+                                {
+                                    side = 0;
+                                    if (Desktop.FromDesktop(Desktop.FromIndex(Desktop.Count - 1)) != 0)
+                                    {
+                                        Desktop.Current.Left.MakeVisible();
+                                        Console.WriteLine("Switching to left virtual desktop");
+                                    }
+                                    else
+                                    {
+                                        SystemSounds.Exclamation.Play();
+                                        Console.WriteLine("No left virtual desktop to switch to.");
+                                    }
+                                }
+                                break;
+                            case "OPC1\n":
+                                /*if (disp != 1)
+                                {
+                                    disp = 1;
+                                    DisplayManager.SetDisplayMode(DisplayManager.DisplayMode.External);
+                                }
+                                else
+                                {
+                                    disp = 0;
+                                    DisplayManager.SetDisplayMode(DisplayManager.DisplayMode.Extend);
+                                }*/
+                                ahk.ExecRaw("SendRaw, %Clipboard%");
+                                break;
+                            case "OPC2\n":
+                                break;
+                                if (disp != 2)
+                                {
+                                    disp = 2;
+                                    DisplayManager.SetDisplayMode(DisplayManager.DisplayMode.Internal);
+                                }
+                                else
+                                {
+                                    disp = 0;
+                                    DisplayManager.SetDisplayMode(DisplayManager.DisplayMode.Extend);
+                                }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    DateTime now = DateTime.Now;
+                    Console.WriteLine(now.ToString("F"));
+                    Console.WriteLine("---");
+
+                    if (e.Message == "The port is closed.")
+                    {
+                        try
+                        {
+                            sp.Open();
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Could not reconnect.");
+                            System.Threading.Thread.Sleep(1000);
+                        }
+                    }
+                    else
+                        SystemSounds.Exclamation.Play();
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+            sp.Close();
+        }
+    }
+}
